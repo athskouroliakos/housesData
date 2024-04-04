b9bb8aa74a5292d4d160fee32f5d5a4459d47963
skouroliakos <thanosmw2@gmail.com>
2024-04-04 14:29:45 +0300

    app

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const houseRoutes = require('./routes/houses');
-const router = require('./routes/houses');
 
 const app = express();
 const PORT = 3001;
 
-router.use(cors());
-router.use('/houses', houseRoutes);
+app.use(cors());
+app.use('/houses', houseRoutes);
 
 //Error handling
-router.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
 
-router.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

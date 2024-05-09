eba8a7a520a37ec48224b28c2fe531f258ce6af4
skouroliakos <thanosmw2@gmail.com>
2024-05-09 16:57:17 +0300

    mod1

--- routes/houses.js
+++ routes/houses.js
@@ -32,8 +32,6 @@ router.post('/', (req, res) => {
   try {
     const newHouse = req.body;
     const newId = uuidv4();
-    newHouse.id = newId;
-    housesData.push(newHouse);
     res.status(201).json(newHouse);
   } catch (error) {
     console.error('Error creating new house:', error);

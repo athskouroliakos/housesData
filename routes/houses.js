19f50b38d96f0c9a8b27c2148417f9b409e69b9b
skouroliakos <thanosmw2@gmail.com>
2024-05-09 14:49:04 +0300

    house by id

--- routes/houses.js
+++ routes/houses.js
@@ -15,4 +15,15 @@ router.get('/', (req, res) => {
   res.json(housesData);
 });
 
+// Fetch a house by its ID
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const house = housesData.find((house) => house.id === parseInt(id));
+  if (house) {
+    res.json(house);
+  } else {
+    res.status(404).json({ message: 'House not found' });
+  }
+});
+
 module.exports = router;

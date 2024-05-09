1fe60d70c0d64e68555ee9c3c9a9cd6b3d2918b7
skouroliakos <thanosmw2@gmail.com>
2024-05-09 14:51:44 +0300

    house by id

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const houseRoutes = require('./routes/houses');
+const houseId = require('./routes/houses/:id');
 
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
 app.use('/houses', houseRoutes);
+app.use('/houses/:id', houseId);
 
 //Error handling
 app.use((err, req, res, next) => {
